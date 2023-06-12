17bffd3fdff95b14290806da54baec325bb1a0b6
rabhamonojoy <mjykoch177@gmail.com>
2023-06-13 01:15:43 +0530

    done

--- index.js
+++ index.js
@@ -1,66 +1,66 @@
 
 
 function roleTyper() {
-    if(i < txt.length){
-      document.getElementById("demo1").innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(roleTyper, speed);
-    }
-    else {
-     
-      i = 0;
-      // clear the text
-      document.getElementById("demo1").innerHTML = "";
-      // make the recursive call
-    
-      setTimeout(roleTyper, speed);
-    }
+  if(i < txt.length){
+    document.getElementById("demo1").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(roleTyper, speed);
   }
+  else {
+   
+    i = 0;
+    // clear the text
+    document.getElementById("demo1").innerHTML = "";
+    // make the recursive call
+  
+    setTimeout(roleTyper, speed);
+  }
+}
 
-  let i=0;
+let i=0;
 let txt="A  NodeJS  Backend  Developer.";
 let speed=100;
 roleTyper()
 
-  
-  var myNav = document.getElementById('nav-menu');
-  var myNav2 = document.getElementById('main');
-  window.onscroll = function () { 
-      if ( document.documentElement.scrollTop >= 15 ) {
-          myNav.classList.add("nav-colored");
-          myNav2.classList.add("nav-colored");
-      } 
-      else {
-          myNav.classList.remove("nav-colored");
-          myNav2.classList.remove("nav-colored");
-      }
-  };
 
-  GitHubCalendar(".calendar", "rabhamonojoy", {
-    responsive: true,
-    global_stats: false,
-    tooltips: true,
-  });
+var myNav = document.getElementById('nav-menu');
+var myNav2 = document.getElementById('main');
+window.onscroll = function () { 
+    if ( document.documentElement.scrollTop >= 15 ) {
+        myNav.classList.add("nav-colored");
+        myNav2.classList.add("nav-colored");
+    } 
+    else {
+        myNav.classList.remove("nav-colored");
+        myNav2.classList.remove("nav-colored");
+    }
+};
 
-  document.getElementById("resume-button-1").onclick=()=>{
-window.open("https://drive.google.com/file/d/1rB2TD3ajaXZVkAI0AiRYiUbglzTaWIvx/view?usp=sharing");
-  }
+GitHubCalendar(".calendar", "rabhamonojoy", {
+  responsive: true,
+  global_stats: false,
+  tooltips: true,
+});
 
-  document.getElementById("resume-button-2").onclick=()=>{
-    window.open("https://drive.google.com/file/d/1rB2TD3ajaXZVkAI0AiRYiUbglzTaWIvx/view?usp=sharing");
-  }
+document.getElementById("resume-button-1").onclick=()=>{
+window.open("https://drive.google.com/file/d/1rB2TD3ajaXZVkAI0AiRYiUbglzTaWIvx/view?usp=drive_link");
+}
 
-  document.getElementById("resume-button-3").onclick=()=>{
-    window.open("https://drive.google.com/file/d/1rB2TD3ajaXZVkAI0AiRYiUbglzTaWIvx/view?usp=sharing");
-  }
-     
-  
-  function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.display = "none";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.display= "block";
-  }
+document.getElementById("resume-button-2").onclick=()=>{
+  window.open("https://drive.google.com/file/d/1rB2TD3ajaXZVkAI0AiRYiUbglzTaWIvx/view?usp=drive_link");
+}
+
+document.getElementById("resume-button-3").onclick=()=>{
+  window.open("https://drive.google.com/file/d/1rB2TD3ajaXZVkAI0AiRYiUbglzTaWIvx/view?usp=drive_link");
+}
+   
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.display = "none";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.display= "block";
+}
